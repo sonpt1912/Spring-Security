e4852b0b1677bcced7399d67affef07ad3fe4c15
Sonpt <phamson191203@gmail.com>
2023-06-19 03:57:03 +0700

    cau hinh form login, va cach login logout

---
+++ form_login_logout/src/main/java/com/example/buoi1/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.buoi1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    /**
+     * trong trường hợp chưa cấu hình lại form login thig
+     * sẽ sử dụng form login mặc định của spring security
+     * */
+
+    @GetMapping("/sonpt")
+    public String home() {
+        return "home";
+    }
+
+}
